0a851afeea90cfbc900b660356ba6198f4d3ae8c
famuvek <32954974+famuvek@users.noreply.github.com>
2018-07-06 16:41:56 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,5 +1,5 @@
 var language = navigator.language || navigator.browserLanguage; //for IE
-if (language == "hu") {
+if (language.includes ('hu')) {
 document.location.href = 'index-hu.html';
 } else {
 document.location.href = 'index-en.html';
